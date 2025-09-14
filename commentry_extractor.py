aad6c7bed3f8156e1b7b78e940b77c19c5338dc3
Sylvester-Anthony <sylvester.anthony.a@gmail.com>
2025-09-14 20:45:20 +0200

    Extract commentary data to text file from match details csv

--- commentry_extractor.py
+++ commentry_extractor.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
 df = pd.read_csv("data/23_24_match_details.csv")
-print(df['events'][1])
+commentary_text = "\n".join(df['events'].astype(str).tolist())
+
+commentary_file = open("/Users/sylvesteranthony/Documents/FC_LLM/data/datacommentary.txt","w")
+commentary_file.write(commentary_text)
+commentary_file.close()
 
